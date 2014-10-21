cff3cffbe57bb4af39f84c1d016163f583b98241
cxoltero <carlosh95@gmail.com>
2014-10-21 16:23:06 -0400

    added the controllers for edit, create, update, destory

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,46 @@
 class TripsController < ApplicationController
+  before_action :find_trip, only: [:show, :edit, :update, :drestroy]
+
+
   def index
+    @trips = Trip.all
   end
 
   def new
     @trip = Trip.new
   end
 
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   def create
     @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to @trip, notice: "Succesfully Created New Trip"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 
 
-private
+  private
 
   def trip_params
     params.require(:trip).permit(:title, :description)
+  end
+
+  def find_trip
+    @trip = Trip.find(params[:id])
   end
 end
