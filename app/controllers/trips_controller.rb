f0b644fc785498c83998c18ceb8c130922027cd1
cxoltero <carlosh95@gmail.com>
2014-10-21 15:23:58 -0400

    created the views for edit, new, form, create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,17 @@ class TripsController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
   end
 
+  def create
+    @trip = Trip.new(trip_params)
+  end
+
+
+private
+
+  def trip_params
+    params.require(:trip).permit(:title, :description)
+  end
 end
